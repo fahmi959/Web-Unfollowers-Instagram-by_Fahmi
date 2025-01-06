30385e2003a18e41b77a8b9916502e2030fc78e6
Fahmi <fahmiardiansyah959@gmail.com>
2025-01-06 09:57:57 +0700

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,25 +1,28 @@
 const admin = require('firebase-admin');
 require('dotenv').config(); // Memuat variabel lingkungan dari .env file
 
-// Mendapatkan kredensial dari variabel lingkungan
+// Pastikan .env file sudah terisi dengan benar
+
 const serviceAccount = {
   type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID, // Jika dibutuhkan, simpan di .env
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Pastikan untuk mengonversi \n ke baris baru
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Pastikan newline di private key dikonversi dengan benar
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID, // Jika diperlukan
+  client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nxwl9%40insta-unfoll-fahmi.iam.gserviceaccount.com'
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL // Variabel ini harus ditambahkan ke .env file Anda jika perlu
 };
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL // URL untuk Realtime Database
+  databaseURL: process.env.FIREBASE_DATABASE_URL // URL untuk Realtime Database Anda
 });
 
+// Mendapatkan referensi ke Realtime Database
 const db = admin.database();
 
 module.exports = db;
