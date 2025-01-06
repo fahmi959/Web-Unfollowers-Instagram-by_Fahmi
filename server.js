c2fff241b819bd5ca30e7d983e850020ab47ebeb
Fahmi <fahmiardiansyah959@gmail.com>
2025-01-06 09:04:32 +0700

    Update and rename app.js to server.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Menggunakan Express hanya untuk file statis dan routing lainnya
+// Routing untuk file statis
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
