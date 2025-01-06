12f4e29d00a2ff77fe934629288878a028b1fc96
Fahmi <fahmiardiansyah959@gmail.com>
2025-01-06 12:28:27 +0700

    Update instagram.js

--- netlify/functions/instagram.js
+++ netlify/functions/instagram.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const axios = require('axios');
-const db = require('../config/firebaseConfig');  // Jalur relatif menuju firebaseConfig.js
+const db = require(path.resolve(__dirname, '../config/firebaseConfig'));
+
 
 const ig = new IgApiClient();
 
