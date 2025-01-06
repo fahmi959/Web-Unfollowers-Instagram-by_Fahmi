85012da3931bc79cb9a6460b8157ab8c765a1056
Fahmi <fahmiardiansyah959@gmail.com>
2025-01-06 10:08:20 +0700

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,25 +1,26 @@
-// /config/firebaseConfig.js
 const admin = require('firebase-admin');
-require('dotenv').config();
 
+// Pastikan variabel lingkungan diatur di Vercel, jadi tidak perlu dotenv
 const serviceAccount = {
   type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Pastikan newline di private key dikonversi dengan benar
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL // Variabel ini harus ditambahkan ke .env file Anda jika perlu
 };
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  databaseURL: process.env.FIREBASE_DATABASE_URL // URL untuk Realtime Database Anda
 });
 
+// Mendapatkan referensi ke Realtime Database
 const db = admin.database();
 
 module.exports = db;
